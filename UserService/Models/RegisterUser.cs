c506f33ed5fa134b01b294bbe8cda05c91cfe705
katja <katja@KATJA>
2026-01-10 00:24:39 +0100

    #API update

---
+++ UserService/Models/RegisterUser.cs
@@ -0,0 +1,8 @@
+﻿namespace UserService.Models
+{
+    public class RegisterUser
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
